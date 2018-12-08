18a7b556a65f6a45665cbcaff28158955a541de6
istiaqhossain <md.istiaqhossain1990@gmail.com>
2018-12-09 01:21:29 +0600

    item status - on progress

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@
 	"use strict";
 
 	building_layout();
+	add_listener();
+
 
 })();
 
@@ -49,7 +51,7 @@ function building_layout(){
 	// list-unstyled
 	create_dom('.itodo--list--wrapper .itodo--list--inner--wrapper','ul',[['class','ifilter--wrapper list--unstyled text-center']]);
 	// li
-	create_dom('.itodo--list--wrapper .itodo--list--inner--wrapper .ifilter--wrapper','li',[['class','all--tasks']]).innerHTML = 'All';
+	create_dom('.itodo--list--wrapper .itodo--list--inner--wrapper .ifilter--wrapper','li',[['class','all--tasks active']]).innerHTML = 'All';
 	// li
 	create_dom('.itodo--list--wrapper .itodo--list--inner--wrapper .ifilter--wrapper','li',[['class','completed--tasks']]).innerHTML = 'Completed';
 	// li
@@ -59,9 +61,74 @@ function building_layout(){
 }
 
 /**
+* Add Listener
+*/
+function add_listener(){
+	
+	let itask_btn = dom('.itask--button')[0];
+
+	itask_btn.addEventListener('click',add_data);
+
+}
+
+/*
+	Store TodoList
+*/
+function set_data(todoList){
+	localStorage.setItem('md_istiaq_hossain_todolist_sunday',JSON.stringify(todoList));
+}
+
+/**
+* Sync Data
+*/
+function sync_data(task,status){
+	let todoList = get_data();
+
+	if(status == 'Add New Task'){
+		todoList.push(task);
+	}
+
+	set_data(todoList);
+}
+
+/**
+* Add Data
+*/
+function add_data(){
+	
+	let request_status = this.innerHTML;
+	let request_data = dom('.itask--name')[0].value;
+
+	if(validateInputs() == false){
+		
+		return false;
+
+	}
+
+	if(request_status == 'Add New Task'){
+
+		let task = {
+			ID : generateID(),
+			task : request_data,
+			status : 'active',
+		};
+		
+		sync_data(task,request_status);
+		
+
+	}
+
+	reset_inputs();
+	reset_list();
+	render_list();
+}
+
+
+/**
  * Render List
  */
 function render_list(){
+	
 	let todoList = get_data();
 
 	if(todoList.length == 0 ){
@@ -69,7 +136,32 @@ function render_list(){
 		create_dom('.itodo--list--wrapper .itodo--list--inner--wrapper','div',[['class','itodo--no--todos text-center']]);
 		// no--todos--content
 		create_dom('.itodo--list--wrapper .itodo--list--inner--wrapper .itodo--no--todos','p',[['class','no--todos--content']]).innerHTML = 'There is no todos';
+	}else{
+		// list-unstyled
+		create_dom('.itodo--list--wrapper .itodo--list--inner--wrapper','ul',[['class','itodo--list--container list--unstyled']]);
+
+		todoList.forEach( function(element, key) {
+			
+			let btn = create_element('button',[['onclick','update_status('+key+')']]);
+			
+			let icon = '<i class="far fa-circle"></i>';
+
+			if(element.status == 'completed'){
+				icon = '<i class="far fa-check-circle"></i>';				
+			}
+			
+			btn.innerHTML = icon;
+
+			let text = document.createTextNode(element.task); 
+
+			// li
+			create_dom('.itodo--list--container','li',[['class','itodo--item--'+element.ID]]);
+
+			dom('.itodo--item--'+element.ID)[0].appendChild(btn);
+			dom('.itodo--item--'+element.ID)[0].appendChild(text);
+		});
 	}
+
 }
 
 /**
@@ -98,11 +190,32 @@ function create_dom(parent, tag, attr = '') {
 
 	if (Array.isArray(attr) && Array.isArray(attr[0])){
 		attr.forEach(function(el){
-			element.setAttribute(el[0], el[1]);
+			if(el[0] && el[1]){
+				element.setAttribute(el[0], el[1]);
+			}
 		});
 	}
 
 	return parent[0].appendChild(element);
+
+}
+
+/**
+ * Create Element 
+ */
+function create_element(tag, attr = '') {
+	
+	let element = document.createElement(tag);
+	
+	if (Array.isArray(attr) && Array.isArray(attr[0])){
+		attr.forEach(function(el){
+			if(el[0] && el[1]){
+				element.setAttribute(el[0], el[1]);
+			}
+		});
+	}
+
+	return element;
 
 }
 
@@ -113,5 +226,84 @@ function create_dom(parent, tag, attr = '') {
 function dom(node) {
 	
 	return document.querySelectorAll(node);
+
+}
+
+/*
+	Validate Inputs
+*/
+function validateInputs(){
+	
+	let flag = true;
+	
+	let task = dom('.itask--name')[0];
+
+	if(task.value.length == 0){
+		
+		flag = false;
+		task.classList.add('field-error');
+
+	}else{
+		
+		task.classList.remove('field-error');
+
+	}
+
+
+	return flag;
+}
+
+/*
+	GENERATE ID
+*/
+function generateID(){
+	
+	return new Date().getTime();
+
+}
+
+/*
+*	Reset Inputs
+*/
+function reset_inputs(){
+	let task = dom('.itask--name')[0];
+
+	task.value = '';
+}
+
+/*
+*	Reset List Wrapper
+*/
+function reset_list(){
+	let no_todos = dom('.itodo--no--todos')[0];
+	let itodos_container = dom('.itodo--list--container')[0];
+
+	if(no_todos){
+		no_todos.remove();	
+	}
+
+	if(itodos_container){
+		itodos_container.remove();
+	}
+	
+}
+
+/*
+*	Update Status of Item
+*/
+function update_status(key){
+	
+	let todoList = get_data();
+
+	if(todoList[key].status == 'active'){
+		todoList[key].status = 'completed';
+	}else{
+		todoList[key].status = 'active';
+	}
+
+	set_data(todoList);
+	reset_inputs();
+	reset_list();
+	render_list();
 
 }
